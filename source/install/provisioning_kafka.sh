74e4851987a26859368cf305f10040e300a9514e
Vincent Tavernier <vincent.tavernier@ensimag.grenoble-inp.fr>
2016-11-26 21:55:56 +0100

    Vagrant machine component selection

---
+++ source/install/provisioning_kafka.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Fail if any command fail
+set -eo pipefail
+
+# This script must be run as root.
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root" 1>&2
+  exit 1
+fi
+
+echo "WARNING: Kafka provisioning not yet implemented" 1>&2
