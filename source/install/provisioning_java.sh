47b59d803230c7856c4488affc47caee1a6fa0a1
Joseph Perez <joseph.perez@ensimag.grenoble-inp.fr>
2016-12-02 17:49:31 +0100

    install jdk instead of jre

--- source/install/provisioning_java.sh
+++ source/install/provisioning_java.sh
@@ -9,7 +9,7 @@ fi
 JAVA_VERSION=$(java -version 2>&1)
 if ! [[ "$JAVA_VERSION" =~ 1\.8 ]]; then
     # Install the default JRE using apt (see debian docs)
-    apt install -y openjdk-8-jre
+    apt install -y openjdk-8-jdk
 else
     echo "Java: already installed."
 fi
