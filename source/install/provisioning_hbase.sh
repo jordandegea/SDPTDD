eda83d003ef862723a73d607315801e6fe5aeb5d
Joseph Perez <joseph.perez@ensimag.grenoble-inp.fr>
2016-12-07 12:44:59 +0100

    Hbase presque fonctionnel

--- source/install/provisioning_hbase.sh
+++ source/install/provisioning_hbase.sh
@@ -10,36 +10,148 @@ else
   source /vagrant/provisioning_shared.sh
 fi
 
-# echo "WARNING: HBase provisioning not yet implemented" 1>&2
+JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+
+HADOOP_VERSION="2.7.3"
+HADOOP_TGZ="hadoop-$HADOOP_VERSION.tar.gz"
+HADOOP_URL="http://apache.mediamirrors.org/common/hadoop-$HADOOP_VERSION/$HADOOP_TGZ"
+HADOOP_HOME="/usr/local/hadoop"
+
+HBASE_VERSION="1.2.4"
+HBASE_LOG_DIR="/var/log/hbase"
+HBASE_TGZ="hbase-$HBASE_VERSION-bin.tar.gz"
+HBASE_URL="http://wwwftp.ciril.fr/pub/apache/hbase/$HBASE_VERSION/$HBASE_TGZ"
+HBASE_HOME="/usr/lib/hbase"
+
+SERVICE_FILE="/etc/systemd/system/hbase.service"
+START_SCRIPT="/usr/local/bin/hstart"
+STOP_SCRIPT="/usr/local/bin/hstop"
+
+# Create the hbase user if necessary
+if ! id -u hbase >/dev/null 2>&1; then
+  echo "HBase: creating user..." 1>&2
+  useradd -m -s /bin/bash hbase
+else
+  echo "HBase: user already created." 1>&2
+fi
+
+# Check the log path for hbase
+if ! [ -d $KAFKA_LOG_DIR ]; then
+  mkdir -p $KAFKA_LOG_DIR
+  chown hbase:hbase -R $KAFKA_LOG_DIR
+fi
+
+# Get options
+IS_MASTER=0
+MASTER=""
+SLAVES=""
+while getopts ":m:s:M" opt; do
+  case "$opt" in
+    m)
+      MASTER="$OPTARG"
+      ;;
+    s)
+      SLAVES="$OPTARG"
+      ;;
+    M)
+      IS_MASTER=1
+      ;;
+    :)
+      echo "Missing argument: the option $opt needs an \
+          argument." >&2
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
+# Download Hadoop
+if (($FORCE_INSTALL)) || ! [ -d $HADOOP_HOME ]
+then
+    echo "Hadoop: Download"
+    get_file $HADOOP_URL $HADOOP_TGZ
+    tar -oxzf $HADOOP_TGZ -C .
+    rm $HADOOP_TGZ
+    rm -rf $HADOOP_HOME
+    mv hadoop* $HADOOP_HOME
+fi
+
+# Configure Hadoop
+echo "Hadoop: Configuration"
+echo "export HADOOP_HOME=$HADOOP_HOME
+export HADOOP_INSTALL=\$HADOOP_HOME
+export HADOOP_MAPRED_HOME=\$HADOOP_HOME
+export HADOOP_COMMON_HOME=\$HADOOP_HOME
+export HADOOP_HDFS_HOME=\$HADOOP_HOME
+export YARN_HOME=\$HADOOP_HOME
+export HADOOP_COMMON_LIB_NATIVE_DIR=\$HADOOP_HOME/lib/native
+export PATH=\$PATH:\$HADOOP_HOME/sbin:\$HADOOP_HOME/bin" > ~/.bashrc
+
+echo "
+export JAVA_HOME=$JAVA_HOME
+" >> $HADOOP_HOME/etc/hadoop/hadoop-env.sh
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<?xml-stylesheet type=\"text/xsl\" href=\"configuration.xsl\"?>
+<configuration>
+  <property>
+    <name>dfs.replication</name>
+    <value>0</value>
+  </property>
+
+  <property>
+    <name>dfs.name.dir</name>
+    <value>file=:///home/hadoop/hadoopdata/hdfs/namenode</value>
+  </property>
+
+  <property>
+    <name>dfs.data.dir</name>
+    <value>file=:///home/hadoop/hadoopdata/hdfs/datanode</value>
+  </property>
+</configuration>" > $HADOOP_HOME/etc/hadoop/hdfs-site.xml
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<?xml-stylesheet type=\"text/xsl\" href=\"configuration.xsl\"?>
+<configuration>
+  <property>
+    <name>fs.default.name</name>
+    <value>hdfs://$MASTER:9OOO</value>
+  </property>
+</configuration>" > $HADOOP_HOME/etc/hadoop/core-site.xml
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<?xml-stylesheet type=\"text/xsl\" href=\"configuration.xsl\"?>
+<configuration>
+  <property>
+    <name>mapreduce.framework.name</name>
+    <value>yarn</value>
+  </property>
+</configuration>" > $HADOOP_HOME/etc/hadoop/mapred-site.xml
 
 # Download HBase
-echo "Download HBase"
-version="1.2.4"
-tgz="hbase-$version-bin.tar.gz"
-url="http://wwwftp.ciril.fr/pub/apache/hbase/$version/$tgz"
-cached_file="/vagrant/resources/$tgz"
-if [ ! -e $cached_file ]
+if (($FORCE_INSTALL)) || ! [ -d $HBASE_HOME ]
 then
-    echo "Downloading $tgz from $url to $cached_file"
-    wget -nv -O $cached_file $url
+    echo "HBase: Download"
+    get_file $HBASE_URL $HBASE_TGZ
+    tar -oxzf $HBASE_TGZ -C .
+    rm $HBASE_TGZ
+    rm -rf $HBASE_HOME
+    mv hbase* $HBASE_HOME
 fi
-libpath="/usr/lib/hbase"
-mkdir -p $libpath
-tar -oxzf $cached_file -C $libpath
+
 
 # Configure HBase
-echo "Configure HBase"
-export HBASE_HOME="/usr/lib/hbase/hbase-$version"
-cd $libpath/hbase-$version/conf
-master="10.20.1.102"
-servers="10.20.1.102, 10.20.1.101, 10.20.1.100"
-echo "<?xml version=\"1.0\"?>
+echo "HBase: Configuration"
+cd $HBASE_HOME/conf
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
 <?xml-stylesheet type=\"text/xsl\" href=\"configuration.xsl\"?>
 <configuration>
  
   <property>
     <name>hbase.rootdir</name>
-    <value>hdfs://$master:9000/hbase</value>
+    <value>hdfs://$MASTER:9000/hbase</value>
   </property>
  
    <property>
@@ -49,27 +161,68 @@ echo "<?xml version=\"1.0\"?>
  
   <property>
     <name>hbase.zookeeper.property.dataDir</name>
-    <value>hdfs://$master:9000/zookeeper</value>
+    <value>hdfs://$MASTER:9000/zookeeper</value>
   </property>
  
   <property>
     <name>hbase.zookeeper.quorum</name>
-    <value>$servers</value>
+    <value>$MASTER $SLAVES</value>
   </property>
  
 </configuration>
 " > hbase-site.xml
 
-JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+rm -f regionservers
+for node in $SLAVES $MASTER; do
+    echo $node >> regionservers
+done
+
 echo "
 export JAVA_HOME=$JAVA_HOME
 " >> hbase-env.sh
 
-# Start hbase
-echo "Start HBase"
-if [ `hostname` = "worker3" ]
-then
-    /usr/lib/hbase/hbase-$version/bin/start-hbase.sh
-fi
-jps
+if (($IS_MASTER)); then
+    echo "Hadoop-Hbase: Master"
+    if (($FORCE_INSTALL)) || ! [ -f $START_SCRIPT ]; then
+        echo "$HADOOP_HOME/sbin/start-dfs.sh
+$HADOOP_HOME/sbin/start-yarn.sh
+$HBASE_HOME/bin/start-hbase.sh" > $START_SCRIPT
+        chmod +x $START_SCRIPT
+    fi
+    if (($FORCE_INSTALL)) || ! [ -f $STOP_SCRIPT ]; then
+        echo "$HADOOP_HOME/sbin/stop-dfs.sh
+$HADOOP_HOME/sbin/stop-yarn.sh
+$HBASE_HOME/bin/stop-hbase.sh" > $STOP_SCRIPT
+        chmod +x $STOP_SCRIPT
+    fi
 
+    # Create the hbase systemd service
+    if (($FORCE_INSTALL)) || ! [ -f $SERVICE_FILE ]; then
+        echo "[Unit]
+Description=Apache HBase
+Requires=network.target
+After=network.target
+
+[Service]
+Type=forking
+User=hbase
+Group=hbase
+Environment=LOG_DIR=$HBASE_LOG_DIR
+ExecStart=$START_SCRIPT
+ExecStop=$STOP_SCRIPT
+Restart=on-failure
+SyslogIdentifier=hbase
+
+[Install]
+WantedBy=multi-user.target" > $SERVICE_FILE
+    fi
+fi
+
+# Reload unit files
+systemctl daemon-reload
+
+if (($IS_MASTER)) && (($ENABLE_VAGRANT)); then
+    $START_SCRIPT
+    # systemctl enable hbase.service
+    # systemctl start hbase.service
+fi
