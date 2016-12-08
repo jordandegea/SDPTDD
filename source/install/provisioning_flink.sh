db3d8b1af2d5394859bd6656bf3ff56b99c339a8
William Duclot <william.duclot@gmail.com>
2016-12-08 18:13:45 +0100

    Provisionning flink qui marche pas lol

--- source/install/provisioning_flink.sh
+++ source/install/provisioning_flink.sh
@@ -5,64 +5,22 @@ set -eo pipefail
 
 # Load the shared provisioning script
 if [ -f './provisioning_shared.sh' ]; then
-  source ./provisioning_shared.sh
+	source ./provisioning_shared.sh
 else
-  source /vagrant/provisioning_shared.sh
+	source /vagrant/provisioning_shared.sh
 fi
 
-# So we dont need to pass in i to the scripts
-NODE_NUMBER=`hostname | tr -d a-z\-`
+filename="flink-1.1.3"
+bindir="/opt/$filename/bin"
 
-
-function downloadFile {
-    filename="flink-1.0.3-bin-hadoop27-scala_2.10.tgz"
-
-    echo "Flink: downloading..."
-
-    get_file "http://www-eu.apache.org/dist/flink/flink-1.0.3/$filename" $filename
-
-    TARBALL=$filename
+function downloadFlink {
+	filename="$filename.tgz"
+	echo "Flink: downloading..."
+	get_file "http://apache.mirrors.ovh.net/ftp.apache.org/dist/flink/flink-1.1.3/flink-1.1.3-bin-hadoop1-scala_2.10.tgz" $filename
 }
-
-
-
-
-while getopts t:r: option; do
-    case $option in
-        t) TOTAL_NODES=$OPTARG;;
-    esac
-done
-
-
-function installFlink {
-
-    downloadFile  $filename
-
-    tar -oxzf $TARBALL -C /opt
-
-    }
-
-function configureFlink {
-    echo "Configuring Flink"
-#set the jobmanager.rpc.address key in conf/flink-conf.yaml to master's IP (worker1)
-    sed -i 's/jobmanager.rpc.address.*/jobmanager.rpc.address:10.20.1.100/' /opt/flink-1.0.3/conf/flink-conf.yaml 
-
-     rm /opt/flink-1.0.3/conf/slaves
-
-#Add the  hostnames of all worker nodes
-   
-       echo "worker2" >> /opt/flink-1.0.3/conf/slaves
-       echo "worker3" >> /opt/flink-1.0.3/conf/slaves
-
-   
-
-
-
-
-}
-
-
 
 echo "Setting up Flink"
-installFlink
-configureFlink
+downloadFlink
+tar -oxzf $filename -C /opt
+$bindir/start-local.sh
+echo "Flink installed"
