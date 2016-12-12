a5ad0345dff08ba63e0ca6c0edfb8449b9e22d78
Vincent Tavernier <vincent.tavernier@ensimag.grenoble-inp.fr>
2016-12-12 12:50:51 +0100

    Fix apt output while provisioning java

--- source/provision/provisioning_java.sh
+++ source/provision/provisioning_java.sh
@@ -6,7 +6,7 @@ source ./provisioning_shared.sh
 JAVA_VERSION=$(java -version 2>&1)
 if ! [[ "$JAVA_VERSION" =~ 1\.8 ]]; then
     # Install the default JRE using apt (see debian docs)
-    apt install -y openjdk-8-jdk
+    apt-get install -y openjdk-8-jdk
 else
     echo "Java: already installed."
 fi
