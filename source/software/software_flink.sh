ace1bf926900f8db6473be61477bdcc4c5556799
Vincent Tavernier <vincent.tavernier@ensimag.grenoble-inp.fr>
2016-12-22 17:02:41 +0100

    Fixed Flink provisioning

--- source/software/software_flink.sh
+++ source/software/software_flink.sh
@@ -9,13 +9,13 @@ source ./deploy_shared.sh
 # Load flink setup parameters
 source ./flink_shared.sh
 
-filename="flink-1.1.3"
+FLINK_BASE_NAME="flink-1.1.3"
 
 # Install Flink
 if (($FORCE_INSTALL)) || ! [ -d $FLINK_INSTALL_DIR ]; then
   echo "Flink: Installing..."
-  get_file "http://apache.mirrors.ovh.net/ftp.apache.org/dist/flink/flink-1.1.3/flink-1.1.3-bin-hadoop1-scala_2.10.tgz" "$filename.tgz"
-  tar xf "$filename.tgz"
+  get_file "http://apache.mirrors.ovh.net/ftp.apache.org/dist/flink/flink-1.1.3/flink-1.1.3-bin-hadoop1-scala_2.10.tgz" "$FLINK_BASE_NAME.tgz"
+  tar xf "$FLINK_BASE_NAME.tgz"
   rm -rf $FLINK_INSTALL_DIR
-  mv $filename $FLINK_INSTALL_DIR
+  mv $FLINK_BASE_NAME $FLINK_INSTALL_DIR
 fi
