dc1ffd9cffa5a8e0f8396a23c83c517880fb89e7
Vincent Tavernier <vincent.tavernier@ensimag.grenoble-inp.fr>
2016-12-26 02:16:41 +0100

    Implemented status command

--- source/apps/ServiceWatcher/service_watcher.py
+++ source/apps/ServiceWatcher/service_watcher.py
@@ -5,8 +5,6 @@ import logging
 from service_watcher.monitor import Monitor
 from service_watcher.status import Status
 
-logging.basicConfig(level=logging.INFO)
-
 parser = argparse.ArgumentParser(description="Twitter Weather ServiceWatcher")
 parser.add_argument('action', metavar='action', choices=['monitor', 'status'],
                     help='the action the script should perform')
@@ -15,6 +13,8 @@ parser.add_argument('--config', dest='config_file', type=argparse.FileType('r'),
 
 args = parser.parse_args()
 if args.action == 'monitor':
+    logging.basicConfig(level=logging.INFO)
     Monitor(args.config_file).run()
 elif args.action == 'status':
+    logging.basicConfig(level=logging.WARNING)
     Status(args.config_file).run()
--- source/apps/ServiceWatcher/service_watcher/status.py
+++ source/apps/ServiceWatcher/service_watcher/status.py
@@ -1,6 +1,8 @@
-import logging
-import signal
+from socket import gethostname
 
+from kazoo.recipe.party import ShallowParty
+
+from service_watcher import service as svc
 from service_watcher.systemd import SystemdClient
 from service_watcher.zookeeper import ZooKeeperClient
 
@@ -13,4 +15,41 @@ class Status(ZooKeeperClient, SystemdClient):
         self.config.setup_systemd(self)
 
     def run(self):
-        pass
+        self.start_zk()
+        self.start_systemd()
+
+        hostname = gethostname()
+        for service in self.config.services:
+            print("%s:" % service.name)
+
+            party = ShallowParty(self.zk, "/service_watcher/active/%s" % service.name)
+            failed_party = ShallowParty(self.zk, "/service_watcher/failed/%s" % service.name)
+
+            if service.type == svc.GLOBAL:
+                print("  type: global")
+            elif service.type == svc.SHARED:
+                print("  type: shared")
+
+            print("  global status:")
+            print("    running on:")
+            instances = 0
+            total_instances = 0
+            for member in party:
+                print("     %s %s" % ('*' if hostname == member else '-', member))
+                instances = instances + 1
+                total_instances = total_instances + 1
+
+            print("    failed on:")
+            for member in failed_party:
+                print("     %s %s" % ('*' if hostname == member else '-', member))
+                total_instances = total_instances + 1
+
+            if service.type == svc.SHARED:
+                total_instances = service.count
+            print("    %d out of %d instances ok" % (instances, total_instances))
+            print("")
+
+
+        self.stop_systemd()
+        self.stop_zk()
+
