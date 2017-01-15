7e2a8df431dd99d72289d9db224b01780e74ff97
Vincent Tavernier <vincent.tavernier@ensimag.grenoble-inp.fr>
2017-01-15 21:48:16 +0100

    Better service_watcher status

--- source/apps/ServiceWatcher/service_watcher/status.py
+++ source/apps/ServiceWatcher/service_watcher/status.py
@@ -1,5 +1,6 @@
 from socket import gethostname
 
+from tabulate import tabulate
 from kazoo.recipe.party import ShallowParty
 
 from service_watcher import service as svc
@@ -11,51 +12,84 @@ class Status(Configurable, ZooKeeperClient):
     def __init__(self, config_file):
         super(Status, self).__init__(config_file=config_file)
 
+    def set_current_service(self, service):
+        self.current_service = service
+        self.is_first_row = True
+
+    def begin_table(self):
+        self.current_table = []
+
+    def begin_row(self):
+        self.end_row()
+
+        if self.is_first_row:
+            type = ["global", "shared", "multi"][self.current_service.type]
+            if hasattr(self.current_service, 'exclusive') and self.current_service.exclusive:
+                type = "%s (exclusive)" % type
+
+            self.current_row = [self.current_service.name, type]
+            self.is_first_row = False
+        else:
+            self.current_row = ["", ""]
+
+    def add_field(self, field):
+        self.current_row.append(field)
+
+    def end_row(self):
+        if self.current_row is not None:
+            self.current_table.append(self.current_row)
+            self.current_row = None
+
+    def out_table(self):
+        self.end_row()
+        print(tabulate(self.current_table, headers=["name", "type", "instance", "running", "failed", "health"]))
+
     def run(self):
         self.config.load()
 
         self.start_zk(self.config.zk_quorum)
 
-        hostname = gethostname()
-        for service in self.config.services:
-            print("%s:" % service.name)
+        # Prepare the output table
+        self.begin_table()
 
-            if service.type == svc.GLOBAL:
-                print("  type: global")
-            elif service.type == svc.SHARED:
-                print("  type: shared")
-            elif service.type == svc.MULTI:
-                print("  type: multi")
+        # Get the status of all services
+        for service in self.config.services:
+            # Set the info for the current service
+            self.set_current_service(service)
 
             if service.type != svc.MULTI:
-                self.print_service_status(hostname, service, service.name)
+                self.begin_row()
+                # Print service status
+                self.print_service_status(service, service.name)
             else:
+                # Print service status for each instance
                 for instance in sorted(service.instances):
-                    self.print_service_status(hostname, service, "%s@%s" % (service.name, instance))
+                    self.begin_row()
+                    self.print_service_status(service, "%s@%s" % (service.name, instance))
 
-            print("")
+        # Print the table
+        self.out_table()
 
         self.stop_zk()
 
-    def print_service_status(self, hostname, service, instance_name):
-        party = ShallowParty(self.zk, "/service_watcher/active/%s" % instance_name)
-        failed_party = ShallowParty(self.zk, "/service_watcher/failed/%s" % instance_name)
+    def print_service_status(self, service, instance_name):
+        party = sorted(list(ShallowParty(self.zk, "/service_watcher/active/%s" % instance_name)))
+        failed_party = sorted(list(ShallowParty(self.zk, "/service_watcher/failed/%s" % instance_name)))
 
-        print("  %s global status:" % instance_name)
-        print("    running on:")
-        instances = 0
-        total_instances = 0
-        for member in sorted(party):
-            print("     %s %s" % ('*' if hostname == member else '-', member))
-            instances = instances + 1
-            total_instances = total_instances + 1
-        print("    failed on:")
-        for member in sorted(failed_party):
-            print("     %s %s" % ('*' if hostname == member else '-', member))
-            total_instances = total_instances + 1
+        # Name of the current service instance
+        self.add_field(instance_name)
+
+        # List of running members
+        self.add_field(", ".join(party))
+
+        # List of failed members
+        self.add_field(", ".join(failed_party))
+
+        # Health status
+        total_instances = len(party) + len(failed_party)
         if service.type == svc.SHARED:
             total_instances = service.count
         elif service.type == svc.MULTI:
             total_instances = service.instances[instance_name.split("@")[1]]
-        print("  summary:")
-        print("    %d out of %d instances ok" % (instances, total_instances))
+
+        self.add_field("%d / %d ok" % (len(party), total_instances))
