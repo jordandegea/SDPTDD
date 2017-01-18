73083bb00122b901c5d04b67906c271ee473bf8a
Vincent Tavernier <vincent.tavernier@ensimag.grenoble-inp.fr>
2017-01-18 22:54:17 +0100

    Fix kafka topics create in configure

--- source/configure/configure_create_topics.sh
+++ source/configure/configure_create_topics.sh
@@ -3,10 +3,16 @@
 
 source ./deploy_shared.sh
 
+EXISTING_TOPICS=$(kafka-topics --list --zookeeper $(hostname):2181)
+
 while getopts ":vft:" arg; do
     case $arg in
         t)
-        kafka-topics --create --zookeeper $(hostname):2181 --replication-factor 3 --partition 1 --topic $OPTARG
+            if ! grep "$OPTARG" <<< "$EXISTING_TOPICS" > /dev/null ; then
+                kafka-topics --create --zookeeper $(hostname):2181 --replication-factor 3 --partition 1 --topic $OPTARG
+            else
+                echo "Topic $OPTARG already exists."
+            fi
         ;;
     esac
 done
