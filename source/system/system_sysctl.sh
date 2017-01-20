1e30ee956d89e0f31171caed335cb73a9f55c256
Vincent Tavernier <vincent.tavernier@ensimag.grenoble-inp.fr>
2017-01-20 18:56:48 +0100

    Make swapfile permanent

--- source/system/system_sysctl.sh
+++ source/system/system_sysctl.sh
@@ -8,6 +8,8 @@ if ! (($ENABLE_VAGRANT)); then
   echo "net.ipv6.conf.all.disable_ipv6 = 1
 net.ipv6.conf.default.disable_ipv6 = 1
 net.ipv6.conf.lo.disable_ipv6 = 1
+
+vm.swappiness=30
 " > /etc/sysctl.conf
   sysctl -p
 
