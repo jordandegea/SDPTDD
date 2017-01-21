e7835b75ad45af41cdd9f0804155b915b51e832b
Vincent Tavernier <vincent.tavernier@ensimag.grenoble-inp.fr>
2017-01-21 14:32:22 +0100

    Higher ulimits for flink

--- source/system/system_sysctl.sh
+++ source/system/system_sysctl.sh
@@ -3,16 +3,20 @@
 # Load the shared provisioning script
 source ./deploy_shared.sh
 
-# Disable IPv6
+# File limits and swappiness
+echo "vm.swappiness=30
+fs.file-max = 500000
+" > /etc/sysctl.conf
+
+# Disable IPv6 on OVH
 if ! (($ENABLE_VAGRANT)); then
   echo "net.ipv6.conf.all.disable_ipv6 = 1
 net.ipv6.conf.default.disable_ipv6 = 1
 net.ipv6.conf.lo.disable_ipv6 = 1
-
-vm.swappiness=30
-" > /etc/sysctl.conf
+" >> /etc/sysctl.conf
   sysctl -p
 
   ip a del $(ifconfig ens3 | awk '/inet6/ {print $3}') dev ens3
-  exit 0
 fi
+
+exit 0
