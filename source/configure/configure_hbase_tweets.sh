73467e1eed5258a4a9bc976425c00e640d630fbe
Jordan DE GEA <jordan.de-gea@grenoble-inp.org>
2017-01-21 17:47:14 +0100

    Fixup: Configuration de HBase

--- source/configure/configure_hbase_tweets.sh
+++ source/configure/configure_hbase_tweets.sh
@@ -6,7 +6,7 @@ source ./hbase_shared.sh
 while getopts ":vft:" arg; do
     case $arg in
         t)
-        echo "create '${OPTARG}_tweets', 'feeling', 'datas'" | $HBASE_HOME/bin/hbase shell
+        echo "create '${OPTARG}_tweets', 'place', 'feeling', 'datas'" | $HBASE_HOME/bin/hbase shell
         ;;
     esac
 done
