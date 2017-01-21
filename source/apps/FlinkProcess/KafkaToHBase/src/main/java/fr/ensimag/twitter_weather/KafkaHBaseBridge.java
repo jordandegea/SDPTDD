e7c1e42e0ab133e15d27b3be38e6fc21e0edb36e
Jordan DE GEA <jordan.de-gea@grenoble-inp.org>
2017-01-21 17:50:48 +0100

    KafkaToHbase

--- source/apps/FlinkProcess/KafkaToHBase/src/main/java/fr/ensimag/twitter_weather/KafkaHBaseBridge.java
+++ source/apps/FlinkProcess/KafkaToHBase/src/main/java/fr/ensimag/twitter_weather/KafkaHBaseBridge.java
@@ -8,6 +8,7 @@ import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSink;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.PrintSinkFunction;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer08;
 import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
 import org.apache.hadoop.hbase.HBaseConfiguration;
@@ -64,6 +65,7 @@ public class KafkaHBaseBridge {
                 parameterTool.getProperties()));
         messageStream.name(String.format("kafka_source_%s", topic));
 
+        //DataStreamSink<String> sink = messageStream.addSink(new ConsoleOutputFormat());
 		/* Passe chaque ligne dans la class HBaseOutputFormat */
         DataStreamSink<String> sink = messageStream.writeUsingOutputFormat(
             new HBaseOutputFormat(
@@ -80,6 +82,41 @@ public class KafkaHBaseBridge {
         public String name;
         public String content;
         public Number feeling;
+    }
+
+
+    /**
+     * Process each line and write in HBase
+     */
+    private static class ConsoleOutputFormat extends PrintSinkFunction<String> {
+
+        @Override
+        public void invoke(String record) {
+            StreamObject so = this.parse(record);
+            record += so.name + so.feeling.toString();
+            super.invoke(record);
+        }
+
+        /* Parse la ligne de kafka pour un sortir un objet pour HBase */
+        public StreamObject parse(String in) {
+            StreamObject obj = null;
+            try {
+                JSONObject json = new JSONObject(in);
+                String text = (String) json.get("text");
+                obj = new StreamObject();
+                obj.name = "randomname";
+                obj.content = in;
+                if (text.contains("\\ud")) {
+                    String substr = text.substring(text.indexOf("\\ud") + 3, text.indexOf("\\ud") + 5);
+                    obj.feeling = Math.abs(Character.digit(substr.charAt(0), 10)) * 10 + Math.abs(Character.digit(substr.charAt(1), 10));
+                }else{
+                    obj.feeling = 50;
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return obj;
+        }
     }
 
     /**
