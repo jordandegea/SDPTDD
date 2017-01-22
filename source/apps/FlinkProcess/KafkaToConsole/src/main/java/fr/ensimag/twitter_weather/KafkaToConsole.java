56525cbb763f45d5293815bd5ae8c0d8148a09ff
Vincent Tavernier <vincent.tavernier@ensimag.grenoble-inp.fr>
2017-01-22 20:34:01 +0100

    Fix KafkaToConsole

--- source/apps/FlinkProcess/KafkaToConsole/src/main/java/fr/ensimag/twitter_weather/KafkaToConsole.java
+++ source/apps/FlinkProcess/KafkaToConsole/src/main/java/fr/ensimag/twitter_weather/KafkaToConsole.java
@@ -54,7 +54,7 @@ public class KafkaToConsole {
 			/* Passe chaque ligne dans la class HBaseOutputFormat */
         messageStream.addSink(new ConsoleOutputFormat());
 
-        env.execute(String.format("Flink console consumer %s", topic));
+        env.execute(String.format("Flink console consumer %s", parameterTool.getRequired("topic")));
     }
 
     public static class StreamObject {
