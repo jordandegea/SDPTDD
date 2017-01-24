1a8d4d66dcd2f3e6f57785f73e9d6ab6ab74d312
Vincent Tavernier <vincent.tavernier@ensimag.grenoble-inp.fr>
2017-01-24 16:44:35 +0100

    Remove Spark

--- source/shared/spark_shared.sh
+++
@@ -1,6 +0,0 @@
-# Spark install parameters
-SPARK_VERSION=1.6.3
-SPARK_NAME=spark-$SPARK_VERSION
-SPARK_FILENAME=spark-${SPARK_VERSION}-bin-hadoop2.6
-SPARK_URL="http://apache.mindstudios.com/spark/spark-${SPARK_VERSION}/${SPARK_FILENAME}.tgz"
-SPARK_INSTALL_DIR="/usr/local/spark"
